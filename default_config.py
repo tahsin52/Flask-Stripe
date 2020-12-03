7d3afec7ece251e4f29a9338a4926dddfb7d0f01
Tahsin52 <tahsin_c@hotmail.com>
2020-12-03 13:43:11 +0300

    first commit

---
+++ default_config.py
@@ -0,0 +1,6 @@
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = "sqlite:///data.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+PROPAGATE_EXCEPTIONS = True
+SECRET_KEY = "sk_test_51HtWOPImJchHSxjterCnM8KLnvCdPE7Pp1jamIxfMOkGq1K7KYrD5t0PJKTACcvn3Ld1On8jUzEH9EOrRNKHDWd000yXIRBIes "
+JWT_SECRET_KEY = "change-this-key-to-something-different-in-the-application-config"
